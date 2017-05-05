7f7035847df9fa5c95c6c295d1d61a0c0ef37f7d
guillaumerolland <guillaume.rolland21@gmail.com>
2017-05-06 00:45:32 +0200

    ajout video ogg ; maj bdd

--- ressources/sql/hyperviseur.sql
+++ ressources/sql/hyperviseur.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 05, 2017 at 10:31 AM
+-- Generation Time: May 06, 2017 at 12:24 AM
 -- Server version: 5.7.18-0ubuntu0.16.04.1
 -- PHP Version: 7.0.15-0ubuntu0.16.04.4
 
@@ -40,7 +40,8 @@ INSERT INTO `entiteCo` (`id`, `nom`) VALUES
 (4, 'ascenseur'),
 (5, 'robot'),
 (1, 'robot'),
-(2, 'porte');
+(2, 'porte'),
+(6, 'maintenance');
 
 -- --------------------------------------------------------
 
@@ -52,19 +53,22 @@ CREATE TABLE `infoEntite` (
   `id` int(10) NOT NULL,
   `vitesse` int(5) NOT NULL,
   `posX` int(5) NOT NULL,
-  `posY` int(5) NOT NULL
+  `posY` int(5) NOT NULL,
+  `disponibilite` tinyint(4) NOT NULL,
+  `chargePortee` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `infoEntite`
 --
 
-INSERT INTO `infoEntite` (`id`, `vitesse`, `posX`, `posY`) VALUES
-(1, 10, -500, 200),
-(2, 25, 500, 80),
-(3, 30, -95, -90),
-(4, 17, -65, -45),
-(5, 6, 20, 20);
+INSERT INTO `infoEntite` (`id`, `vitesse`, `posX`, `posY`, `disponibilite`, `chargePortee`) VALUES
+(1, 10, -500, 200, 1, 0),
+(2, 25, 500, 80, 1, 0),
+(3, 30, -95, -90, 1, 0),
+(4, 17, -65, -45, 1, 0),
+(5, 6, 20, 20, 1, 0),
+(6, 0, 0, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -76,19 +80,21 @@ CREATE TABLE `logAlarm` (
   `id` int(10) NOT NULL,
   `type` varchar(25) NOT NULL,
   `description` varchar(50) NOT NULL,
-  `date` date DEFAULT NULL
+  `date` date DEFAULT NULL,
+  `time` time NOT NULL,
+  `info` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `logAlarm`
 --
 
-INSERT INTO `logAlarm` (`id`, `type`, `description`, `date`) VALUES
-(1, 'alarme', 'description de l\'alarme', '2017-05-04'),
-(2, 'log', 'description du log', '2017-05-04'),
-(3, 'alarme', 'alarme de sécurité', '2017-05-04'),
-(4, 'log', 'robot truc s\'est connecté', '2017-05-04'),
-(5, 'alarme', 'eboulement niveau cavite 3', '2017-05-04');
+INSERT INTO `logAlarm` (`id`, `type`, `description`, `date`, `time`, `info`) VALUES
+(1, 'alarme', 'description de l\'alarme', '2017-05-04', '00:00:00', 'informations'),
+(2, 'log', 'description du log', '2017-05-04', '00:00:00', 'informations'),
+(3, 'alarme', 'alarme de sécurité', '2017-05-04', '00:00:00', 'informations'),
+(4, 'log', 'robot truc s\'est connecté', '2017-05-04', '00:00:00', 'informations'),
+(5, 'alarme', 'eboulement niveau cavite 3', '2017-05-04', '00:00:00', 'informations');
 
 -- --------------------------------------------------------
 
