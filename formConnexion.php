c9c15163d06254738c512167bcdd6315af733a72
guillaumerolland <guillaume.rolland21@gmail.com>
2017-05-08 21:04:10 +0200

    session

---
+++ formConnexion.php
@@ -0,0 +1,7 @@
+<h1>connexion</h1>
+
+<form class="" action="connexion.php" method="post">
+  <input type="text" name="login" value="" placeholder="contenu">
+  <input type="password" name="pwd" value="" placeholder="password">
+  <input type="submit" name="" value="connexion">
+</form>
