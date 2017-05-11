885dabcccdf7ea55e7ef4d923e190a6d22d34a55
guichard-erwan56 <guichard.erwan56@gmail.com>
2017-05-11 18:17:41 +0200

    config 2.0, 2.1, 2.2, 2.3, 2.4

--- serveur/moduleDemo/demoLinkBdd.php
+++ serveur/moduleDemo/demoLinkBdd.php
@@ -25,8 +25,8 @@
 	  
 	}
 
-	//config 2 : config1 + prépare les entités à superviser
-	public function config2(){
+	//config 2.0 : crée les utilisateurs, connecte les entités à superviser, leur donnes des infos
+	public function config2.0(){
 		
 		reinitialise();
 
@@ -40,22 +40,16 @@
 		$requete->execute();
 		
 		$query = "INSERT INTO `entiteCo` (`nom`) values
-	      	('robot'),
-      		('ascenseur'),
-      		('robot'),
-      		('robot'),
-      		('porte'),
-      		('maintenance');";
+	      	('robot0'),
+      		('robot1'),
+      		('robot2');";
 		$requete = $bdd->prepare($query);
 		$requete->execute();
 		
 		$query = "INSERT INTO `infoEntite` (vitesse, `posX`, `posY`, `disponibilite`, `chargePortee`) VALUES
     		(10, -500, 200, 1, 0),
     		(25, 500, 80, 1, 0),
-    		(30, -95, -90, 1, 0),
-    		(17, -65, -45, 1, 0),
-    		(6, 20, 20, 1, 0),
-    		(0, 0, 0, 1, 0);
+    		(30, -95, -90, 1, 0);
     		";
 		$requete = $bdd->prepare($query);
 		$requete->execute();
@@ -64,45 +58,13 @@
 		
 	}
 	
-	//config 3 : config2 + crée des alarmes
-	public function config3(){
+	//le robot 1 crée une alerte de niveau 1
+	public function config2.1(){
 		
 		$bdd = coBaseDonnee::getConnection();
 		
-		$query ="INSERT INTO `utilisateur` (`login`, `pwd`, `admin`) VALUES
-			('aze', 'aze', 1),
-			('qsd', 'qsd', 0);
-			";
-		$requete = $bdd->prepare($query);
-		$requete->execute();
-		
-		$query = "INSERT INTO `entiteCo` (`nom`) values
-	      	('robot'),
-      		('ascenseur'),
-      		('robot'),
-      		('robot'),
-      		('porte'),
-      		('maintenance');";
-		$requete = $bdd->prepare($query);
-		$requete->execute();
-		
-		$query = "INSERT INTO `infoEntite` (vitesse, `posX`, `posY`, `disponibilite`, `chargePortee`) VALUES
-    		(10, -500, 200, 1, 0),
-    		(25, 500, 80, 1, 0),
-    		(30, -95, -90, 1, 0),
-    		(17, -65, -45, 1, 0),
-    		(6, 20, 20, 1, 0),
-    		(0, 0, 0, 1, 0);
-    		";
-		$requete = $bdd->prepare($query);
-		$requete->execute();
-		
-		$query ="INSERT INTO `logAlarm` ( `type`, `description`, `date`, `time`, `info`, sender, niveau) VALUES
-    		('alarme', 'description de l\'alarme', '2017-05-04', '00:00:00', 'informations', 1, 2),
-    		('log', 'description du log', '2017-05-04', '00:00:00', 'informations', 2, 1),
-    		('alarme', 'alarme de sécurité', '2017-05-04', '00:00:00', 'informations', 1, 2),
-    		('log', 'robot truc s\'est connecté', '2017-05-04', '00:00:00', 'informations', 3, 3),
-    		('alarme', 'eboulement niveau cavite 3', '2017-05-04', '00:00:00', 'informations', 2, 1);
+		$query = "INSERT INTO `logAlarm`(type,description,date,time,info,traitee,niveau,sender) VALUES
+			('batterie','niveau de batterie à 25%',:date,:time,'',0,1,1);
     		";
 		$requete = $bdd->prepare($query);
 		$requete->execute();
@@ -110,6 +72,74 @@
 		return "OK";
 		
 	}
+	
+	//le robot 2 se met en indisponible et crée une alerte de niveau 3
+	public function config2.2(){
+		
+		$bdd = coBaseDonnee::getConnection();
+		
+		$query = "UPDATE `infoEntite`
+			SET disponibilite = 0
+    		WHERE id = 2;
+    		";
+		$requete = $bdd->prepare($query);
+		$requete->execute();
+		
+		$query = "INSERT INTO `logAlarm`(type,description,date,time,info,traitee,niveau,sender) VALUES
+			('alarme','moteur endommagé',:date,:time,'',0,3,2);
+    		";
+		$requete = $bdd->prepare($query);
+		$requete->execute();
+		
+		return "OK";
+		
+	}
+	
+	//le problème est réglé, le robot 2 se remet en disponible et l'alerte est traitée
+	public function config2.3(){
+		
+		$bdd = coBaseDonnee::getConnection();
+		
+		$query = "UPDATE `infoEntite`
+			SET disponibilite = 1
+    		WHERE id = 2;
+    		";
+		$requete = $bdd->prepare($query);
+		$requete->execute();
+		
+		$query = "UPDATE `logAlarm`
+			SET traitée = 1
+			WHERE sender = 2;
+    		";
+		$requete = $bdd->prepare($query);
+		$requete->execute();
+		
+		return "OK";
+		
+	}
+	
+	//le robot 1 est retiré de la liste des robots, en attente de son remplacement
+	public function config2.4(){
+		
+		$bdd = coBaseDonnee::getConnection();
+		
+		$query = "DELETE FROM `infoEntite`
+    		WHERE id = 1;
+    		";
+		$requete = $bdd->prepare($query);
+		$requete->execute();
+		
+		$query = "DELETE FROM `entiteCo`
+    		WHERE id = 1;
+    		";
+		$requete = $bdd->prepare($query);
+		$requete->execute();
+		
+		return "OK";
+		
+	}
+	
+	
 	
     public function makeAction ($action, $id) {
       $bdd = coBaseDonnee::getConnection();
@@ -118,7 +148,7 @@
 
       if ($action == "avarie") {
         $query = "
-          insert into logAlarm (type, description, info, date, time, traitee, sender)
+          insert into logAlarm(type, description, info, date, time, traitee, sender)
           values (
             'alarme',
             'description avarie',
@@ -179,7 +209,8 @@
       $requete = $bdd->prepare($query);
       $requete->execute();
 
-      $query = "CREATE TABLE entiteCo(id int (10) AUTO_INCREMENT PRIMARY KEY,
+      $query = "CREATE TABLE entiteCo(
+		id int (10) AUTO_INCREMENT PRIMARY KEY,
         nom varchar(25) NOT NULL)
         ENGINE=InnoDB DEFAULT CHARSET=latin1;";
       $requete = $bdd->prepare($query);
@@ -188,12 +219,14 @@
       
 
       $query = "CREATE TABLE `infoEntite` (
-		  `id` int(10) AUTO_INCREMENT PRIMARY KEY,
+		  `id` int(10),
 		  `vitesse` int(5) NOT NULL,
 		  `posX` int(5) NOT NULL,
 		  `posY` int(5) NOT NULL,
 		  `disponibilite` tinyint(4) NOT NULL,
-		  `chargePortee` int(11) NOT NULL
+		  `chargePortee` int(11) NOT NULL,
+		  PRIMARY KEY (id),
+		  FOREIGN KEY (id) REFERENCES EntiteCo(id)
 		) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
       $requete = $bdd->prepare($query);
       $requete->execute();
@@ -232,9 +265,9 @@
 
       $query ="CREATE TABLE `utilisateur` (
 		  `id` int(10) AUTO_INCREMENT PRIMARY KEY,
-      login varchar(20) not null,
-      pwd varchar(20) not null,
-      admin int(4) default 0
+		login varchar(20) not null,
+		pwd varchar(20) not null,
+		admin int(4) default 0
 		) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 		";
       $requete = $bdd->prepare($query);
